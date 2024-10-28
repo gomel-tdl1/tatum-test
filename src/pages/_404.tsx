d2a999957b3873b152a757ced187823942c1b0e3
Papa_Carlo <rock_cell@mail.ru>
2024-10-28 10:54:05 +0100

    fix: favicon feat: 404 page

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,8 +1,14 @@
+import { FadeInWhenVisible } from "@/components/fades";
+
 export function NotFound() {
   return (
-    <section>
-      <h1>404: Not Found</h1>
-      <p>It&apos;s gone :(</p>
-    </section>
+    <FadeInWhenVisible>
+      <section className="flex flex-col items-center pt-60">
+        <h1 className="text-[24px] font-bold bg-gradient-to-br from-gradient_from_1 to-gradient_to_1 text-transparent bg-clip-text">
+          404: Not Found
+        </h1>
+        <p>It&apos;s gone :(</p>
+      </section>
+    </FadeInWhenVisible>
   );
 }
